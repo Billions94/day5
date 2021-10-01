b131f48ee2f39eb1d9cea09dedb3a4948957a8b5
Billions94 <E.a.egbedi@gmail.com>
2021-10-01 13:34:46 +0200

    commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
 import MyNav from './components/MyNav'
 import MyRow from './components/MyRow';
+import Body from './components/Body'
 
 function App() {
   return (
     <div className="App">
       <MyNav />
-      <MyRow query={'harry%20potter'}/>
+      <Body />
     </div>
   );
 }
