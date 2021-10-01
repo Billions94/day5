2eae68c00dbfaba59e39c146fd921360c9b711ae
Billions94 <E.a.egbedi@gmail.com>
2021-10-01 11:01:43 +0200

    commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import MyNav from './components/MyNav'
+import MyRow from './components/MyRow';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <MyNav />
+      <MyRow query={'harry%20potter'}/>
     </div>
   );
 }
